173bc60b9fbe840f9886dfe0e017185405d0e05f
Nguyen Quang Phuc <nguyen.quang.phuc@linecorp.com>
2024-11-15 12:12:22 +0700

    change picture

--- product/backend/database.ts
+++ product/backend/database.ts
@@ -5,28 +5,52 @@ const db = new Datastore();
 db.insert(
   [
     {
-      name: "Porsche-Diesel Master 419",
-      image: "http://localhost:3000/images/tractor-red.jpg",
-      thumb: "http://localhost:3000/images/tractor-red-thumb.jpg",
+      name: "Charizard",
+      image: "http://localhost:3000/images/charizard.avif",
+      thumb: "http://localhost:3000/images/charizard.avif",
       price: "66.00",
       currency: "USD",
-      type: 'tractor'
+      type: 'fire'
     },
     {
-      name: "Fendt F20 Dieselroß",
-      image: "http://localhost:3000/images/tractor-green.jpg",
-      thumb: "http://localhost:3000/images/tractor-green-thumb.jpg",
+      name: "Dragonite",
+      image: "http://localhost:3000/images/dragonite.avif",
+      thumb: "http://localhost:3000/images/dragonite.avif",
       price: "54.00",
       currency: "USD",
-      type: 'tractor'
+      type: 'dragon'
     },
     {
-      name: "Eicher Diesel 215/16",
-      image: "http://localhost:3000/images/tractor-blue.jpg",
-      thumb: "http://localhost:3000/images/tractor-blue-thumb.jpg",
+      name: "Eevee",
+      image: "http://localhost:3000/images/eevee.avif",
+      thumb: "http://localhost:3000/images/eevee.avif",
       price: "58.00",
       currency: "USD",
-      type: 'tractor'
+      type: 'normal'
+    },
+    {
+      name: "Gengar",
+      image: "http://localhost:3000/images/gengar.avif",
+      thumb: "http://localhost:3000/images/gengar.avif",
+      price: "24.00",
+      currency: "USD",
+      type: 'ghost'
+    },
+    {
+      name: "Gyarados",
+      image: "http://localhost:3000/images/gyarados.avif",
+      thumb: "http://localhost:3000/images/gyarados.avif",
+      price: "99.00",
+      currency: "USD",
+      type: 'water'
+    },
+    {
+      name: "Pikachu",
+      image: "http://localhost:3000/images/pikachu.avif",
+      thumb: "http://localhost:3000/images/pikachu.avif",
+      price: "88.00",
+      currency: "USD",
+      type: 'electric'
     },
   ],
   (err) => {
